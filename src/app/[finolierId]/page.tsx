965906d090930de08aeab13ae9dc6049034b3a1a
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-05 16:43:37 +0200

    Minor styling changes

--- src/app/[finolierId]/page.tsx
+++ src/app/[finolierId]/page.tsx
@@ -21,16 +21,18 @@ export default async function FinolierPage({
   return (
     <div className="flex flex-col items-center justify-center mt-8">
       <Image
-        className="rounded-xl"
+        className="border-2 border-slate-700 rounded-full"
         src={finolier.avatar}
-        width={80}
-        height={80}
+        width={100}
+        height={100}
         alt="avatar"
       />
       <h1 className="text-4xl mb-4 font-bold">{finolier.displayName}</h1>
-      {finolier.about && finolier.location && <h2 className="text-2xl mb-4 italic">
-        {finolier.about}. {finolier.location}
-      </h2>}
+      {finolier.about && finolier.location && (
+        <h2 className="text-2xl mb-4 italic">
+          {finolier.about}. {finolier.location}
+        </h2>
+      )}
       <h2 className="text-xl mb-2">{finolier.numPosts} comentarios</h2>
       <h2 className="text-xl mb-2">
         {finolier.numFollowers} seguidores, siguiendo a {finolier.numFollowing}
