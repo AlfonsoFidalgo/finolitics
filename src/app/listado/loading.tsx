e287b6249888b2ca2e08ba03b4e04ef2ca12662d
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-06 17:35:42 +0200

    Replace spinner

--- src/app/listado/loading.tsx
+++ src/app/listado/loading.tsx
@@ -1,7 +1,9 @@
+import Spinner from "@/components/UI/spinner";
+
 export default function Loading() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-slate-700"></div>
+      <Spinner />
     </div>
   );
 }
