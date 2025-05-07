7dee523ebf5713b875088a3aa0e776e3dcdd1ff6
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-07 09:47:48 +0200

    Style homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 export default async function Home() {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center mt-8">
-        <p className="text-gray-500 text-sm mt-2">
-          Finolitics. Tu herramienta de confianza para saber más sobre tus
-          finoliers favoritos y perder aún más el tiempo.
-        </p>
-      </div>
+    <div className="flex flex-col items-center justify-center mt-8">
+      <h1 className="text-xl mt-2 w-11/12 sm:w-8/12">
+        Finolitics, tu herramienta de confianza para saber más sobre tus
+        finoliers favoritos y poder perder aún más el tiempo.
+      </h1>
     </div>
   );
 }
