a5d79ccad89370e1e6ef86fe29e5e2cbaf2b9eee
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-07 09:16:52 +0200

    Responsive header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,22 +33,22 @@ export default function Header() {
   }, [setUserId, userId]);
 
   return (
-    <header className="bg-slate-800 text-white p-4 flex items-center justify-between">
-      <h1 className="text-3xl font-bold font-mono tracking-wider uppercase">
-        <Link href="/" className="text-white hover:text-gray-300">
+    <header className="bg-slate-800 text-white sm:p-4 py-4 flex items-center justify-between w-full">
+      <h1 className="text-xl sm:text-2xl font-bold font-mono tracking-wider uppercase">
+        <Link href="/" className="text-white hover:text-gray-30 pl-2">
           Finolitics
         </Link>
       </h1>
       <div className="flex gap-8 mx-8">
         <Link
           href="/buscar"
-          className="text-lg font-semibold hover:text-gray-300 transition duration-300"
+          className="sm:text-lg font-semibold hover:text-gray-300 transition duration-300"
         >
           Buscar
         </Link>
         <Link
           href="/listado"
-          className="ml-4 text-lg font-semibold hover:text-gray-300 transition duration-300"
+          className="sm:text-lg font-semibold hover:text-gray-300 transition duration-300"
         >
           Listado
         </Link>
