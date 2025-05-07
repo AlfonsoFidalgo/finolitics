408b920bfdaf79985e4bc7a01578c6c01b09355f
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-07 11:30:13 +0200

    Add dummy table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import {
+  fetchTopReputationFinoliers,
+  fetchTopComentators,
+  fetchTopUpvoted,
+} from "@/actions/utils";
+import Table from "@/components/UI/table";
+
 export default async function Home() {
+  // const topReputationFinoliers = await fetchTopReputationFinoliers();
+  // const topComentators = await fetchTopComentators();
+  const topUpvoted = await fetchTopUpvoted();
+
   return (
-    <div className="flex flex-col items-center justify-center mt-8">
-      <h1 className="text-xl mt-2 w-11/12 sm:w-8/12">
-        Finolitics, tu herramienta de confianza para saber más sobre tus
-        finoliers favoritos y poder perder aún más el tiempo.
-      </h1>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center mt-8">
+        <h1 className="text-xl mt-2 w-11/12 sm:w-8/12">
+          Finolitics, tu herramienta de confianza para saber más sobre tus
+          finoliers favoritos y poder perder aún más el tiempo.
+        </h1>
+      </div>
+      <div className="mt-8 w-full sm:w-8/12 mx-auto">
+        <Table data={topUpvoted} />
+      </div>
+    </>
   );
 }
