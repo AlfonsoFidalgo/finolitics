70d0a1eed1e2d13d1e4b52792ee901e0827d1ba9
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-12 11:42:49 +0200

    Improve userId handling

--- src/hooks/useStoreUserId.ts
+++ src/hooks/useStoreUserId.ts
@@ -1,38 +1,53 @@
 import { type Dispatch, type SetStateAction, useEffect } from "react";
-import { saveUser, fetchUser } from "@/actions/utils";
+import { saveUser, isUserInDB } from "@/actions/utils";
 
 function generateUserId() {
-  return `user_${Math.random().toString(36).substr(2, 9)}`;
+  return `${Math.random().toString(36).slice(2)}`;
 }
 
 export default function useStoreUserId(
+  userIdLocalStorage: string | null,
   setUserId: Dispatch<SetStateAction<string | null>>
 ) {
   useEffect(() => {
+    let isMounted = true;
+
     async function handleUserStorage() {
+      console.log("in handleUserStorage");
+
       if (typeof window === "undefined" || !window.localStorage) return;
       try {
-        let storedUserId = window.localStorage.getItem("userId");
-        if (storedUserId) {
-          const resUserId = await fetchUser(storedUserId);
-          if (!resUserId.success) {
-            storedUserId = null;
-          }
+        let newUserId: string | null = userIdLocalStorage;
+        let userInDB = false;
+        if (newUserId) {
+          // userId exists in localStorage, we need to check if it's in the DB
+          userInDB = await isUserInDB(newUserId);
         }
-        if (!storedUserId) {
-          storedUserId = generateUserId();
-          window.localStorage.setItem("userId", storedUserId);
+
+        if (!newUserId || !userInDB) {
+          // userId doesn't exist in localStorage or not in DB, generate a new one
+          console.log(
+            "No userId found in localStorage or not in DB, generating a new one"
+          );
+
+          newUserId = generateUserId();
+          window.localStorage.setItem("userId", newUserId);
           try {
-            await saveUser(storedUserId);
+            await saveUser(newUserId);
           } catch (error) {
             console.error("Failed to save user:", error);
           }
         }
-        setUserId(storedUserId);
+        if (isMounted) {
+          setUserId(newUserId);
+        }
       } catch (error) {
         console.error("Error handling user storage:", error);
       }
     }
     handleUserStorage();
-  }, [setUserId]);
+    return () => {
+      isMounted = false;
+    };
+  }, [userIdLocalStorage, setUserId]);
 }
