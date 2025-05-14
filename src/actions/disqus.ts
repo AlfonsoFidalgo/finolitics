c695c63d022dd369dd9905bfcd257152d8c3d6ed
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-14 15:56:18 +0200

    Add fetch thread details action

--- src/actions/disqus.ts
+++ src/actions/disqus.ts
@@ -94,3 +94,56 @@ export async function fetchFinolierDetails(
     finolier: finolierData,
   };
 }
+
+interface Thread {
+  id: string;
+  title: string;
+  link: string;
+}
+
+export async function fetchThreadDetails(id: string) {
+  const url = `https://disqus.com/api/3.0/threads/details?thread=${id}&api_key=${process.env.DISQUS_API}`;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    return {
+      success: false,
+      message: "Thread no encontrado",
+      thread: {} as Thread,
+    };
+  }
+  const data = await res.json();
+  const threadData = {
+    id: data.response.id,
+    title: data.response.clean_title,
+    link: data.response.link,
+  };
+
+  try {
+    const existingThread = await prisma.threads.findUnique({
+      where: { id: threadData.id },
+    });
+
+    if (!existingThread) {
+      await prisma.threads.create({
+        data: { ...threadData },
+      });
+      return {
+        success: true,
+        message: "Thread details fetched successfully",
+        thread: threadData,
+      };
+    }
+  } catch (error) {
+    console.error("Error fetching thread:", error);
+    return {
+      success: false,
+      message: "Error storing the thread",
+      thread: {} as Thread,
+    };
+  }
+}
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
-import prisma from "@/db";
+
+import { fetchThreadDetails } from "@/actions/disqus";
 
 export async function GET() {
   const headersList = await headers();
@@ -14,45 +15,5 @@ export async function GET() {
     });
   }
 
-  const url = `https://disqus.com/api/3.0/threads/details?thread=10538477384&api_key=${process.env.DISQUS_API}`;
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (!res.ok) {
-    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-  const data = await res.json();
 
-  const threadPayload = {
-    id: data.response.id,
-    title: data.response.clean_title,
-    link: data.response.link,
-  };
-
-  try {
-    await prisma.threads.create({
-      data: threadPayload,
-    });
-    return new Response(JSON.stringify({ message: "All good" }), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error }), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
 }
