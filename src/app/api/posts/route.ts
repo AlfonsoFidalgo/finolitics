7e6ef36a89382ccc25657de0ec26038aab22fb35
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-14 11:50:22 +0200

    Add basic cron job to fetch threads

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,62 @@
-import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import prisma from "@/db";
 
 export const config = {
-  schedule: "* * * * *", // Every day at 9:00 AM UTC
+  schedule: "* * * * *",
 };
 
 export async function GET() {
-  return NextResponse.json({ ok: "all good man" });
+  const headersList = await headers();
+  if (
+    headersList.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const url = `https://disqus.com/api/3.0/threads/details?thread=10538477384&api_key=${process.env.DISQUS_API}`;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  const data = await res.json();
+
+  const threadPayload = {
+    id: data.response.id,
+    title: data.response.clean_title,
+    link: data.response.link,
+  };
+
+  try {
+    prisma.threads.create({
+      data: threadPayload,
+    });
+    return new Response(JSON.stringify({ message: "All good" }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
