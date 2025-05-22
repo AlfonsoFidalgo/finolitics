0e46dc95af4bf705e3498a166e90a1152a21b6e6
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-22 11:27:18 +0200

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -39,6 +40,7 @@ export default function RootLayout({
           </p>
           <Analytics />
         </UserContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
