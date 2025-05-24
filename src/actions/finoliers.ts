3e81ec5f1df70d635462a09f4332a885c6b5faa4
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-05-24 11:18:12 +0200

    Reduce fetching

--- src/actions/finoliers.ts
+++ src/actions/finoliers.ts
@@ -217,7 +217,7 @@ export async function fetchNonPrivateFinoliers(): Promise<Finolier[]> {
       orderBy: {
         reputation: "desc",
       },
-      take: 100,
+      take: 50,
     });
 
     return finoliers;
